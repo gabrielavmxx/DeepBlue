2e21fef32b62f5929d7c9912cad72984034d49ba
gabrielavmxx <gabriela.montero@sptech.school>
2025-05-06 23:58:30 -0300

    Alterações no css

--- database/deepblue.sql
+++ database/deepblue.sql
@@ -8,3 +8,19 @@ create table usuario
     email varchar(60),
     senha varchar(20),
 );
+
+create table quiz
+(
+    idQuz int auto_increment primary key,
+    descricao varchar(50)
+);
+
+create table resultado_quiz
+(
+    idResultado int primary key auto_increment,
+    resultado varchar(40),
+    fkUsuario int,
+    fkQuiz int,
+    foreign key (fkUsuario) references usuario (idUsuario),
+    foreign key (fkQuiz) references quiz (idQuiz)
+);
