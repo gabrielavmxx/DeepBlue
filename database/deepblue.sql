e7ddcc26263776fb14fcdf30c209fc3128b2e347
gabrielavmxx <gabriela.montero@sptech.school>
2025-05-25 14:18:16 -0300

    database modificado

--- database/deepblue.sql
+++ database/deepblue.sql
@@ -1,4 +1,4 @@
-
+drop database deepblue;
 create database deepblue;
 use deepblue;
 
@@ -27,9 +27,30 @@ create table resultado_quiz
     idResultado int primary key auto_increment,
     resultado char(1),
     correto boolean,
+    numQuestao int,
     fkUsuario int,
     fkQuiz int,
     dataQuiz datetime default CURRENT_TIMESTAMP,
     foreign key (fkUsuario) references usuario (idUsuario),
     foreign key (fkQuiz) references quiz (idQuiz)
 );
+
+select * from usuario;
+use deepblue;
+select * from resultado_quiz;
+
+select rq.correto from usuario u inner join resultado_quiz rq on u.idUsuario = rq.fkUsuario where idUsuario = 1 and fkQuiz = 2 and correto = 1 group by idUsuario desc limit 7;
+
+SELECT rq.correto
+FROM resultado_quiz rq
+INNER JOIN usuario u ON u.idUsuario = rq.fkUsuario
+WHERE rq.fkUsuario = 1 AND rq.fkQuiz = 2 AND rq.correto = 1
+ORDER BY rq.idResultado DESC
+LIMIT 7;
+
+SELECT numQuestao, COUNT(*) AS totalAcertos
+        FROM resultado_quiz
+        WHERE correto = 1
+        AND fkQuiz = 3
+        GROUP BY numQuestao
+        ORDER BY numQuestao;
